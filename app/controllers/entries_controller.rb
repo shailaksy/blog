2d55db18538050d44023b2f9e440476c47e43a6d
shailaksy <shailaksy@gmail.com>
2022-12-01 20:41:33 +0800

    added entry params

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -65,6 +65,6 @@ class EntriesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def entry_params
-      params.fetch(:entry, {})
+      params.require(:entry).permit(:title, :author, :body, :published)
     end
 end
